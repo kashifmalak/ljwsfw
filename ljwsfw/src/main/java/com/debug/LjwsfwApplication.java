a8f8c71ed9e6c5a070c06310f6130f342436b2c6
Kashif Malak <kashifmalak@yahoo.com>
2015-10-14 15:42:40 +0300

    dddf

---
+++ ljwsfw/src/main/java/com/debug/LjwsfwApplication.java
@@ -0,0 +1,12 @@
+package com.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjwsfwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjwsfwApplication.class, args);
+    }
+}
---
+++ ljwsfw/src/main/java/com/debug/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.debug;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LjwsfwApplication.class);
+	}
+
+}
